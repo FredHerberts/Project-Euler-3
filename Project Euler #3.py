6a780fc73c48202d8ce7c041739c27240b6b9bab
FredHerberts <51488709+FredHerberts@users.noreply.github.com>
2019-06-08 13:17:31 +0200

    Update Project Euler #3.py

--- Project Euler #3.py
+++ Project Euler #3.py
@@ -1,25 +1,23 @@
 import math
 import time
-
-primelist = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101]
-primelist2 = [3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101]
+primelist = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
+primelist2 = [3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
 Factors = []
-
 number = int(600851475143)
-
 start = time.time()
 
-for x in primelist:
-    factor = True
-    while factor == True:
-        if number % x != 0:
-            factor = False
+def primefactoring(number):
+    for x in primelist:
+        factor = True
+        while factor == True:
+            if number % x != 0:
+                factor = False
+                break
+            Factors.append(x)
+            number = int(number/x)
+        if x > number+ 1:
             break
-        Factors.append(x)
-        number = number / x
-
-def primefinder(number):
-    for y in range(103, number + 1,2):
+    for y in range(103, number + 1, 2):
         prime = True
         for x in primelist2:
             if y % x == 0:
@@ -36,17 +34,14 @@ def primefinder(number):
                     break
                 Factors.append(y)
                 number = number / y
-                if y > number+1:
+                if y > number + 1:
                     break
             if y > number + 1:
                 break
         if y > number + 1:
             break
 
-
-primefinder(int(number))
-end = time.time()
+primefactoring(int(number))
 print(Factors)
+end = time.time()
 print(end - start)
-
-
